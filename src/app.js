989cd639357cb9525a5df9d2f2b3aa5d77a2e222
mwcz <mwc@clayto.com>
2015-03-15 17:10:18 -0400

    output json to screen

--- src/app.js
+++ src/app.js
@@ -15,13 +15,31 @@ function set_transform(func) {
 }
 
 function create() {
-    set_transform(default_transform);
+    set_transform( transforms.random );
     timeout_id = setInterval(update, 500);
 }
 
 function update() {
-    var newstate = transform(state.current());
-    console.log(newstate);
+
+    // get latest input
+    var newinput = input.read();
+
+    // pass it to the transform function
+    var newdata = transform({
+        depth  : newinput.depth,
+        traits : newinput.traits,
+        state  : state.current(),
+    });
+
+    // pass the transformed data to the renderer
+
+    // the renderer is just raw json for now :)
+    var output = JSON.stringify(newdata, null, 4)
+    .replace(/\[\n\s+(\d+)/g, '[ $1')  // make the
+    .replace(/(\d+)\n\s+\]/g, '$1 ]')  // json pretty print
+    .replace(/\,\n\s+(\d+)/g, ', $1'); // prettier
+
+    document.getElementById('output').innerText = output;
 }
 
 function teardown() {
