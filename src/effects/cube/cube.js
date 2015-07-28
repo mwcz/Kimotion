57218c9b012037282f0b8454c5913435eeb3ab04
mwcz <mwc@clayto.com>
2015-07-28 14:02:13 -0400

    improve cube rotation smoothness

--- src/effects/cube/cube.js
+++ src/effects/cube/cube.js
@@ -7,21 +7,35 @@ let geometry;
 let cube;
 let material;
 
-export default class particles {
+export default class cube_effect {
     constructor(_gfx) {
         gfx = _gfx;
         scene    = gfx.gl.scene;
 
         geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+        for ( let i = 0; i < geometry.faces.length; i += 2 ) {
+            let hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex( hex );
+            geometry.faces[ i + 1 ].color.setHex( hex );
+        }
+
+        material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
         cube = new THREE.Mesh( geometry, material );
-        gfx.gl.camera.lookAt(cube.position);
         scene.add( cube );
-        gfx.gl.camera.position.z = 5;
+        gfx.gl.camera.lookAt(cube.position);
+        gfx.gl.camera.position.z = 2;
+
+        this.prev_dsum = 0;
     }
     update(gfx) {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        let dsum = 0;
+        for (let i = 0; i < gfx.depth.length; i += 1) {
+            dsum += gfx.depth[i];
+        }
+        let dsum_diff = (dsum - this.prev_dsum) / 5e7;
+        cube.rotation.y += dsum_diff;
+        this.prev_dsum = dsum;
     }
     destroy() {}
 }
