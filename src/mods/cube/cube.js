20f0e58f0ff0d8bb3e88f4c6464a91b1974a1502
mwcz <mwc@clayto.com>
2015-08-04 12:40:20 -0400

    set cube size based on motion

--- src/mods/cube/cube.js
+++ src/mods/cube/cube.js
@@ -6,8 +6,38 @@ export default class particles extends mod {
         this.author = 'Michael Clayton';
         this.title = 'Cube';
         this.add_effect('cube');
+
+        for ( let i = 0; i < gfx.gl.geometry.faces.length; i += 2 ) {
+            let hex = Math.random() * 0xffffff;
+            gfx.gl.geometry.faces[ i ].color.setHex( hex );
+            gfx.gl.geometry.faces[ i + 1 ].color.setHex( hex );
+        }
+
+        this.last_depth = gfx.depth;
+        this.scale = 1;
     }
     update(gfx) {
+
+        let scale = 0.1; // new depth gets this much weight
+        let dsum = 0;
+
+        for (let i = 0; i < gfx.depth.length; i += 1) {
+            dsum += Math.abs(this.last_depth[i] - gfx.depth[i]);
+        }
+
+        this.scale = (1-scale) * this.scale + scale * Math.log(Math.log(dsum+1)+1);
+
+        gfx.gl.cube.rotation.x += 0.001;
+        gfx.gl.cube.rotation.y += 0.01;
+        gfx.gl.cube.rotation.z += 0.0001;
+
+        gfx.gl.cube.scale.setX(this.scale);
+        gfx.gl.cube.scale.setY(this.scale);
+        gfx.gl.cube.scale.setZ(this.scale);
+
+        this.last_depth = gfx.depth;
+
         super.update(gfx);
+
     }
 }
