111487505006eea15651fbeef9f734d8e4808f1e
Jared Sprague <jsprague@redhat.com>
2015-09-04 22:03:06 -0400

    adding multi-fish support

--- src/mods/fish/fish.js
+++ src/mods/fish/fish.js
@@ -20,9 +20,15 @@ function Sprite (img_path, img_height) {
     this.getInfo = function() {
         return this.img_path + ' ' + this.img_height + ' ' + this.x + ' ' + this.y;
     };
+    this.resetOffScreen = function() {
+        this.x = 2400;
+        this.y = random(10, height - this.img_height);
+    }
 }
 
-var fish1 = new Sprite('mods/fish/assets/fish1.png', 200);
+var fishes = [];
+fishes.push(new Sprite('mods/fish/assets/fish1.png', 200));
+fishes.push(new Sprite('mods/fish/assets/fish1.png', 200));
 
 var coins = [];
 
@@ -41,8 +47,10 @@ export default class fishMod extends mod {
         this.author = 'Jared Sprague';
         this.title = 'Fish';
 
+        // initialize all the fishes inital positions, direction, speed
+        this.initFish();
+
         water_img = loadImage("mods/fish/assets/underwater1.jpg");
-        fish1.img = loadImage(fish1.img_path);
         hand_img = loadImage("mods/fish/assets/hand.png");
         coin_img = loadImage("mods/fish/assets/coin.png");
 
@@ -50,23 +58,21 @@ export default class fishMod extends mod {
         console.log("height: " + height);
 
         background(water_img); 
-
-        fish1.x = 2400;
-        fish1.y = random(10, height - fish1.img_height);
-
-        console.log(fish1.getInfo());
     }
 
     update(gfx) {
         clear(); // clear the screen to draw the new frame
         background(water_img);
 
-        // update call coin positions
+        // update all coin positions
         this.updateCoins();
 
-        image(fish1.img, fish1.x -= 10, fish1.y);
+        // update all fish positions
+        this.updateFish();
+
         image(hand_img, gfx.hand.x, gfx.hand.y);
 
+/*
         if (this.detectCatch(gfx.hand.x, gfx.hand.y, fish1.x, fish1.y)) {
 	    console.log("FISH CAUGHT!");
 
@@ -88,17 +94,12 @@ export default class fishMod extends mod {
             // add the coin to the coins array
             coins.push(newCoin);
 	}
-
-        if (fish1.x <= -400) {
-	    fish1.x = 2400;
-            fish1.y = random(10, height - fish1.img_height);
-        }
+*/
 
         super.update(gfx);
     }
 
     detectCatch(hand_x, hand_y, fish_x, fish_y) {
-        //TODO: make this better
         if (Math.abs(hand_x - fish_x) <= 100 && Math.abs(hand_y - fish_y) <= 100 ) {
             return true;
         }
@@ -109,6 +110,26 @@ export default class fishMod extends mod {
         for (let coin of coins) {
             if (coin.y > -300) {
                 image(coin_img, coin.x, coin.y -= 20);
+            }
+        }
+    }
+
+    initFish() {
+        for (let fish of fishes) {
+            // Load their image
+            fish.img = loadImage(fish.img_path);
+            fish.resetOffScreen();
+        }
+    }
+
+    updateFish() {
+        for (let fish of fishes) {
+            // draw and move the fish
+            image(fish.img, fish.x -= 10, fish.y);
+
+            // if offscreen reset
+            if (fish.x <= -400) {
+                fish.resetOffScreen();
             }
         }
     }
