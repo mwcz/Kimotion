866f3e30d239aba53a7232c297394b01e32add6e
mwcz <mwc@clayto.com>
2015-09-08 12:22:18 -0400

    have spiral mod set uniforms only on slider update

--- src/mods/spiral/spiral.js
+++ src/mods/spiral/spiral.js
@@ -1,5 +1,6 @@
 import THREE from 'threejs';
 import mod from 'mod';
+import { partial } from 'lodash';
 import * as frag from 'text!./shaders/particle.frag';
 import * as vert from 'text!./shaders/vertex.vert';
 
@@ -12,20 +13,24 @@ export default class spiral extends mod {
 
         gfx.conf.gui.add(params, 'spiral_strength', 0, 500.0)
             .step(1)
-            .name('Spiral Strength');
+            .name('Spiral Strength')
+            .onChange(partial(this.updateUniform, gfx));
 
         this.author = 'Kevin Howell';
         this.title = 'Spiral';
         this.add_effect('particles');
+        this.updateUniform(gfx);
         gfx.gl.particles.material.vertexShader = vert;
         gfx.gl.particles.material.fragmentShader = frag;
         gfx.gl.particles.material.blending = THREE.AdditiveBlending;
     }
-    update(gfx) {
+    updateUniform(gfx) {
         gfx.gl.particles.system.material.uniforms.spiral_strength = {
             type : 'f',  value : params.spiral_strength
         };
         gfx.gl.particles.system.material.needsUpdate = true;
+    }
+    update(gfx) {
         super.update(gfx);
     }
 }
