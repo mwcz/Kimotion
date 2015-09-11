d6a17ca82de24537aa7e0b5f156a845f492f907f
mwcz <mwc@clayto.com>
2015-09-10 21:36:51 -0400

    update graph mod to track hand position

--- src/mods.js
+++ src/mods.js
@@ -1,15 +1,1 @@
-export { default as colorcycle } from './mods/colorcycle/colorcycle';
-export { default as cube } from './mods/cube/cube';
-export { default as example } from './mods/example/example';
-export { default as example2d } from './mods/example2d/example2d';
-export { default as fish } from './mods/fish/fish';
-export { default as flow_field } from './mods/flow_field/flow_field';
 export { default as graph } from './mods/graph/graph';
-export { default as handcube } from './mods/handcube/handcube';
-export { default as handtrack } from './mods/handtrack/handtrack';
-export { default as particles } from './mods/particles/particles';
-export { default as sandstorm } from './mods/sandstorm/sandstorm';
-export { default as snake } from './mods/snake/snake';
-export { default as spiral } from './mods/spiral/spiral';
-export { default as storm } from './mods/storm/storm';
-export { default as torus } from './mods/torus/torus';
--- src/mods/graph/graph.js
+++ src/mods/graph/graph.js
@@ -1,16 +1,48 @@
+/* jshint ignore:start */
+
 import THREE from 'threejs';
 import mod from 'mod';
 
-const PAD = 10;
-const DIA = 3;
-const PADDIA = PAD + DIA;
+const PAD = { x: 400, y: 100};
+const DIA = 8;
+const PADDIA = {
+    x: PAD.x + DIA,
+    y: PAD.y + DIA,
+};
+const PADDIA2 = {
+    x: PAD.x + DIA*2,
+    y: PAD.y + DIA*2,
+};
+
+let CLEAR_RECT = {};
+let DRAW_RECT = {};
 
 export default class graph extends mod {
     constructor(gfx) {
         super(gfx);
-        gfx.set(this, '2d');
+
         this.author = 'Michael Clayton';
         this.title = 'Graph';
+
+        // use 2d mode (p5.js)
+        gfx.set(this, '2d');
+
+        // handtracking!
+        this.add_effect('handtracking2d');
+
+        CLEAR_RECT = {
+            x: PADDIA.x,
+            y: PADDIA.y,
+            w: width - PADDIA.x*2,
+            h: height - PADDIA.y*2,
+        };
+
+        DRAW_RECT = {
+            x: PADDIA2.x,
+            y: PADDIA2.y,
+            w: width - PADDIA2.x*2,
+            h: height - PADDIA2.y*2,
+        };
 
         background(255);
 
@@ -22,28 +54,38 @@ export default class graph extends mod {
         // create "handrawn" axis lines
 
         // x axis
-        for( var x = PAD; x < width - PAD; x += 1 ) {
-            ellipse( x, height - PAD + noise(x), DIA, DIA);
+        // for( var x = PAD; x < width - PAD; x += (width - 2*PAD) / HANDDRAWN_SEGMENTS ) {
+        for( var x = PAD.x; x < width - PAD.x; x += 3 ) {
+            ellipse( x, height - PAD.y + noise(x), DIA, DIA);
         }
 
         // y axis
-        for( var y = PAD; y < width - PAD; y += 1 ) {
-            ellipse( PAD + noise(y), y, DIA, DIA);
+        for( var y = PAD.y; y < height - PAD.y; y += 3 ) {
+            ellipse( PAD.x + noise(y), y, DIA, DIA);
         }
 
+        this.ticker = 0;
     }
     update(gfx) {
-        fill(255, 255, 255, 0.1);
-        // clear out the graph area
-        rect(PADDIA, PADDIA, width - 2*PADDIA, height - 2*PADDIA);
 
-        // draw a sine wave
+        this.ticker += 1;
+
+        if (this.ticker === 4) {
+            fill(255,255,255,8);
+            // clear out the graph area
+            rect(CLEAR_RECT.x, CLEAR_RECT.y, CLEAR_RECT.w, CLEAR_RECT.h);
+            this.ticker = 0;
+        }
+
         fill(0);
-        // var xpos = width * second() / 60;
-        // rect( xpos, height/2 + 14*sin(xpos), DIA, DIA);
 
-        var xpos = PADDIA + (width-PADDIA*2) * (millis()%1000)/1000;
-        ellipse( xpos, height/2 + 100*sin(xpos*2), DIA, DIA);
+        let dot_x = DRAW_RECT.x + DRAW_RECT.w * (gfx.hand.x / width);
+        let dot_y = DRAW_RECT.y + DRAW_RECT.h * (gfx.hand.y / height);
+
+        ellipse( dot_x, dot_y, DIA, DIA);
+
         super.update(gfx);
     }
 }
+
+/* jshint ignore:end */
