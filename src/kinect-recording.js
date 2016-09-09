ddbc95e59fd289dbf5ecc87e2c2e42fc4dc98159
mwcz <mwc@clayto.com>
2016-09-08 21:43:09 -0400

    s/var/let|const

--- src/kinect-recording.js
+++ src/kinect-recording.js
@@ -1,12 +1,13 @@
 const kinect_recording = (recording_file) => {
-    let file = recording_file || 'recordings/kinect/handtracking.bin'
     console.log('Using Kinect recording data source');
 
-    var i     = 0;
-    var step  = conf.kinect.res.width * conf.kinect.res.height;
+    const file = recording_file || 'recordings/kinect/handtracking.bin'
 
-    var depth = new Uint16Array(step);
-    var recording = new Uint16Array(depth.length);
+    const step  = conf.kinect.res.width * conf.kinect.res.height;
+    let i     = 0;
+
+    let depth = new Uint16Array(step);
+    let recording = new Uint16Array(depth.length);
 
     function read() {
         return { depth };
