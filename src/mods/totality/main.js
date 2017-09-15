ae2cd8ad64c27acd0c9fadd72f3bf6ca22c205ee
Michael Clayton <mclayton@redhat.com>
2017-09-15 00:46:56 -0400

    add hand position marker to totality mod

--- src/mods/totality/main.js
+++ src/mods/totality/main.js
@@ -21,17 +21,32 @@ class totality extends mod {
         this.radius = 100;
         this.coronaLength = 200;
 
+        this.marker = document.createElement('div');
+        this.marker.style.background = 'white';
+        this.marker.style.borderRadius = '50%';
+        this.marker.style.width = this.marker.style.height = '8px';
+        this.marker.style.position = 'absolute';
+        this.markerPosition = { x: 0, y: 0 };
+        document.body.appendChild(this.marker);
+
         background(0); // white
         fill(color(255, 1)); // white
-        stroke(color(255, 8)); // black borders
+        stroke(color(255, 2)); // black borders
         strokeWeight(2);
+
+        this.origin = {
+            x: width/2,
+            y: height/2 - height/6
+        };
     }
     update(gfx) {
 
         if (gfx.data.leapFrame && gfx.data.leapFrame.hands.length) {
-            translate(width/2, height/2 - height/6);
+
+            // draw line tangent to the eclipse circle, with length increasing as hand moves away from center point
+            translate(this.origin.x, this.origin.y);
             const hand = gfx.data.hand;
-            const handPoint = createVector(hand.x - width/2, hand.y - height/2);
+            const handPoint = createVector(hand.x - this.origin.x, hand.y - this.origin.y);
             const tangentPoint = handPoint.copy().normalize().mult(this.radius);
 
             const coronaLength = this.coronaLength + handPoint.mag();
@@ -42,6 +57,11 @@ class totality extends mod {
             coronaPointB.add(tangentPoint);
 
             line( coronaPointA.x, coronaPointA.y, coronaPointB.x, coronaPointB.y);
+
+            this.markerPosition.x = hand.x * 0.05 + this.markerPosition.x * 0.95;
+            this.markerPosition.y = hand.y * 0.05 + this.markerPosition.y * 0.95;
+            this.marker.style.left = this.markerPosition.x+'px';
+            this.marker.style.top  = this.markerPosition.y+'px';
         }
 
         super.update(gfx);
